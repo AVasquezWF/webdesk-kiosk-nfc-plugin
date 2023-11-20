23feeb7809be2939c3b005e47eb135992c5853ae
Adrian Vasquez <adrian.vasquez@workflow.at>
2023-11-20 14:43:02 +0100

    a

--- nfcPlugin/src/main/java/WebdeskKioskNFCPlugin.java
+++ nfcPlugin/src/main/java/WebdeskKioskNFCPlugin.java
@@ -1,6 +1,5 @@
 import android.content.Context;
 
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -28,11 +27,44 @@ public class WebdeskKioskNFCPlugin extends CordovaPlugin {
         System.out.println(data);
         System.out.println(callbackContext);
 
+        switch (action){
+            case "init": return StartReader();
+            case "checkIsReady": return StartReader();
+            case "addListener": return StartReader();
+            default: return false;
+        }
+    }
+
+    private boolean StartReader() {
+        String TYPE_ID = "02";
+        String TYPE_IC = "01";
 
         RS485Util rfid = new RS485Util(context);
         rfid.setCOM("/dev/ttyS7");
+        if (rfid == null) {
+            return false;
+        }
+
         rfid.start();
+        rfid.getData((buffer, size) -> {
+            byte[] data = new byte[size];
+            for (int i = 0; i < size; i++) {
+                data[i] = buffer[i];
+            }
+            String hexData = HexUtil.ByteArrToHex(data).replace(" ", "");
+            System.out.println(hexData);
+            String cardType = hexData.substring(4, 6);
+            String cardData = null;
+            if (cardType.equals(TYPE_ID)) {
+                cardData = hexData.substring(8, hexData.length() - 4);
+            } else if (cardType.equals(TYPE_IC)) {
+                cardData = hexData.substring(6, hexData.length() - 4);
+            }
+            System.out.println(cardData);
+        });
         return true;
+
     }
+
 
 }
