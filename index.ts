46a10254ecf19995a526683f76b47f8084d0669f
Adrian Vasquez <adrian.vasquez@workflow.at>
2023-11-22 12:31:16 +0100

    a

--- index.ts
+++ index.ts
@@ -3,19 +3,42 @@ import { exec } from "cordova";
 const pluginName = "WebdeskKioskNFCPlugin";
 enum Methods {
     checkIsReady = "checkIsReady",
+    poolReadTag = "poolReadTag",
     addListener = "addListener",
     init = "init",
 }
-const useKioskReader = {
-    checkIsReady: (success: any, error: any) => {
-        exec(success, error, pluginName, Methods.checkIsReady, []);
-    },
-    addListener: (success: any, error: any) => {
-        exec(success, error, pluginName, Methods.addListener, []);
-    },
-    init: (success: any, error: any) => {
-        exec(success, error, pluginName, Methods.init, []);
-    },
+
+type UseExecTemplate = (success: () => void, error: () => void) => void;
+interface KioskReaderPlugin {
+    checkIsReady: UseExecTemplate;
+    poolReadTag: UseExecTemplate;
+    addListener: UseExecTemplate;
+    init: UseExecTemplate;
+}
+
+const useKioskReader = (): KioskReaderPlugin => {
+    let poolReadTagId: number | NodeJS.Timeout = 0;
+    const useExec =
+        (method: Methods): UseExecTemplate =>
+        (success: any, error: any) => {
+            exec(success, error, pluginName, method, []);
+        };
+
+    const checkIsReady = useExec(Methods.checkIsReady);
+    const poolReadTag: UseExecTemplate = (success, error) => {
+        const executor = useExec(Methods.poolReadTag);
+        clearInterval(poolReadTagId);
+        poolReadTagId = setInterval(() => executor(success, error), 2000);
+    };
+    const addListener = useExec(Methods.addListener);
+    const init = useExec(Methods.init);
+
+    return {
+        checkIsReady,
+        poolReadTag,
+        addListener,
+        init,
+    };
 };
 
-(window as any).useKioskReader = useKioskReader;
+(window as any).useKioskReader = useKioskReader();
--- www/index.js
+++ www/index.js
@@ -5,18 +5,28 @@ const pluginName = "WebdeskKioskNFCPlugin";
 var Methods;
 (function (Methods) {
     Methods["checkIsReady"] = "checkIsReady";
+    Methods["poolReadTag"] = "poolReadTag";
     Methods["addListener"] = "addListener";
     Methods["init"] = "init";
 })(Methods || (Methods = {}));
-const useKioskReader = {
-    checkIsReady: (success, error) => {
-        (0, cordova_1.exec)(success, error, pluginName, Methods.checkIsReady, []);
-    },
-    addListener: (success, error) => {
-        (0, cordova_1.exec)(success, error, pluginName, Methods.addListener, []);
-    },
-    init: (success, error) => {
-        (0, cordova_1.exec)(success, error, pluginName, Methods.init, []);
-    },
+const useKioskReader = () => {
+    let poolReadTagId = 0;
+    const useExec = (method) => (success, error) => {
+        (0, cordova_1.exec)(success, error, pluginName, method, []);
+    };
+    const checkIsReady = useExec(Methods.checkIsReady);
+    const poolReadTag = (success, error) => {
+        const executor = useExec(Methods.poolReadTag);
+        clearInterval(poolReadTagId);
+        poolReadTagId = setInterval(() => executor(success, error), 2000);
+    };
+    const addListener = useExec(Methods.addListener);
+    const init = useExec(Methods.init);
+    return {
+        checkIsReady,
+        poolReadTag,
+        addListener,
+        init,
+    };
 };
-window.useKioskReader = useKioskReader;
+window.useKioskReader = useKioskReader();
